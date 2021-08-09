7493a32133884355a77a842b5f7bd4912d09f8a8
Bala Chandrudu Bondigala <78945152+BalaChandrudu409@users.noreply.github.com>
2021-08-09 12:42:23 +0530

    Changed Name

--- src/com/mindtree/HelloWorld.java
+++ src/com/mindtree/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World Mounika Bondigala");
-		System.out.println("New Branch Bala");
+		System.out.println("New Branch Bala Added");
+		
 	}
 
 }
